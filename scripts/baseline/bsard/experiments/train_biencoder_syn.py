5749b69024b1c720a09edef51917f077c1cd0fd1
MURONG Yanchao <yanchao.murong@docaposte.fr>
2024-04-07 13:04:56 +0200

    train script adapt

--- scripts/baseline/bsard/experiments/train_biencoder_syn.py
+++ scripts/baseline/bsard/experiments/train_biencoder_syn.py
@@ -263,21 +263,21 @@ class BiEncoderTrainer(object):
                 # Update global step.
                 global_step += 1
 
-            # Save the model.
-            if epoch == self.epochs - 1:
-                self.model.save(join(self.output_path, f"{epoch}"))
+            # Report average loss and number of correct predictions.
+            print(
+                f'Epoch {epoch}: Train loss {(train_loss / num_batches):>8f} - Accuracy {(train_correct / num_samples * 100):>0.1f}%')
 
             if (epoch + 1) % 10 == 0:
-                # Evaluate model after each epoch.
+                self.model.save(join(self.output_path, f"{epoch}"))
+
+                # Evaluate model after each 10 epoch.
                 scores = self.evaluator(model=self.model, device=self.device, batch_size=self.batch_size * 3,
                                         epoch=epoch,
                                         writer=self.writer)
 
                 pprint.pprint(f'Evaluation: {scores}')
 
-            # Report average loss and number of correct predictions.
-            print(
-                f'Epoch {epoch}: Train loss {(train_loss / num_batches):>8f} - Accuracy {(train_correct / num_samples * 100):>0.1f}%')
+
 
 
 
