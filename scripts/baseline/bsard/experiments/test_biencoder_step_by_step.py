dea5bd1741c57b5ab137fab85a941ace0a292909
MURONG Yanchao <yanchao.murong@docaposte.fr>
2024-05-09 09:11:03 +0200

    fix test step by step script

--- scripts/baseline/bsard/experiments/test_biencoder_step_by_step.py
+++ scripts/baseline/bsard/experiments/test_biencoder_step_by_step.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     model = BiEncoder.load(checkpoint_path)
 
     # 2. Load the test set.
+    print(args.test_queries_df_path)
     test_queries_df = pd.read_csv(args.test_queries_df_path)
     documents_df = pd.read_csv(abspath(join(__file__, "../../../bsard/data/articles_fr.csv")))
     test_dataset = BSARDataset(test_queries_df, documents_df)
